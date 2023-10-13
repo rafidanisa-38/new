abb61037283a516ee583248c49af27a10d3c3246
Rafida Nisa <78836288+rafidanisa-38@users.noreply.github.com>
2023-10-13 23:01:20 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let plus = document.querySelector(".plus");
+let box = document.querySelector(".box");
+let min = document.querySelector(".min");
+
+plus.onclick = function() {
+	plus.classList.toggle("active");
+	box.classList.toggle("active");
+}
+
+min.onclick = function() {
+	min.classList.toggle("active");
+	box.classList.toggle("active");
+}
+
+
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 // Get the navbar
@@ -12,35 +27,3 @@ function myFunction() {
 		navbar.classList.toggle("sticky", window.scrollY > 0);
 	}
 }
-
-
-// let text = document.getElementById('text');
-
-// window.addEventListener('scroll', () => {
-// 	let value = window.scrollY;
-
-// 	text.style.marginTop = value * 0 + 'px';
-
-// });
-
-// const header = document.querySelectorAll(".header-main");
-
-// header.forEach((item, index) => {
-// 	let 
-// 	console.log(item);
-
-// })
-
-// let listElements = document.querySelectorAll('.link');
-// listElements.forEach(listElement => {
-// 	listElement.addEventListener('click', () => {
-// 		if(listElement.classList.contains('active')){
-// 			listElement.classList.remove('active');
-// 		} else {
-// 			listElements.forEach(ListE => {
-// 				ListE.classList.remove('active');
-// 			})
-// 			listElement.classList.toggle('active');
-// 		}
-// 	})
-// })
