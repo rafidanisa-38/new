313dbbcab1dc3bf5ac21f775c1ec2f131a677687
Rafida Nisa <78836288+rafidanisa-38@users.noreply.github.com>
2023-10-24 15:21:23 +0700

    script.js

--- script.js
+++ script.js
@@ -27,3 +27,53 @@ function myFunction() {
 		navbar.classList.toggle("sticky", window.scrollY > 0);
 	}
 }
+
+
+
+
+
+const text=document.getElementById("quotes");
+const author=document.getElementById("author");
+
+const getNewQuote = async () =>
+{
+    //api for quotes
+    var url="https://type.fit/api/quotes";    
+
+    // fetch the data from api
+    const response=await fetch(url);
+    console.log(typeof response);
+    //convert response to json and store it in quotes array
+    const allQuotes = await response.json();
+
+    // Generates a random number between 0 and the length of the quotes array
+    const indx = Math.floor(Math.random()*allQuotes.length);
+
+    //Store the quote present at the randomly generated index
+    const quote=allQuotes[indx].text;
+
+    //Store the author of the respective quote
+    const auth=allQuotes[indx].author;
+
+    if(auth==null)
+    {
+        author = "Anonymous";
+    }
+
+    //function to dynamically display the quote and the author
+    text.innerHTML=quote;
+    author.innerHTML="~ "+auth;
+}
+getNewQuote();
+
+
+const check = document.getElementById('check');
+check.addEventListener('click', () => {
+	check.style.display = 'none';
+
+	const qtitle = document.getElementById('quoteTitle');
+	qtitle.style.display = 'none';
+
+	const qbtn = document.getElementById('qbtn');
+	qbtn.style.display = 'none';
+})
