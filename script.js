e0baffc9689e5ddbbad1b0cadecb37c083549c10
Rafida Nisa <78836288+rafidanisa-38@users.noreply.github.com>
2023-11-01 10:27:46 +0700

    script.js

--- script.js
+++ script.js
@@ -68,6 +68,13 @@ getNewQuote();
 
 
 const check = document.getElementById('check');
+
+const qdecor1 = document.querySelector('.quote-decoration-1');
+const qdecor2 = document.querySelector('.quote-decoration-2');
+const qdecor3 = document.querySelector('.quote-decoration-3');
+const qdecor4 = document.querySelector('.quote-decoration-4');
+const qdecor5 = document.querySelector('.quote-decoration-5');
+
 check.addEventListener('click', () => {
 	check.style.display = 'none';
 
@@ -76,4 +83,19 @@ check.addEventListener('click', () => {
 
 	const qbtn = document.getElementById('qbtn');
 	qbtn.style.display = 'none';
+	
+	qdecor1.style.transform = 'translate(-33%, -60%)';
+	qdecor1.style.transform += 'scale(.6)';
+
+	qdecor2.style.transform = 'translate(-33%, -45%)';
+	qdecor2.style.transform += 'scale(.8)';
+
+	qdecor3.style.transform = 'translate(10%, -50%)';
+	qdecor3.style.transform += 'scale(.7)';
+
+	qdecor4.style.transform = 'translate(0%, 75%)';
+	qdecor4.style.transform += 'scale(.6)';
+
+	qdecor5.style.transform = 'translate(30%, 80%)';
+	qdecor5.style.transform += 'scale(.7)';
 })
